7a9a46668368795731513edcf076257ee07865ea
chinesejar <chinesejar@outlook.com>
2018-04-08 15:38:35 +0800

    modify config.py

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ from platform import platform
 
 
 DB_HOST = '127.0.0.1'
-if 'fedora' in platform():
-    DB_HOST = '120.92.80.41'
 DB_PORT = 3306
-DB_USERNAME = 'root'
-DB_PASSWORD = 'Zhujia1991'
+DB_USERNAME = 'username'
+DB_PASSWORD = 'password'
 DB_NAME = 'aqi'
